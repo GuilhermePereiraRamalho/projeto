15153a870ef5c0c44b8389ef50aa22031c0c5666
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-02-10 17:21:13 +0100

    Ajustando views e urls e criando template de visualizacao de receita

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Guilherme Ramalho'
+    })
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Guilherme Ramalho'
+    })
