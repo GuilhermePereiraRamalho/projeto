29f15fd9477abb684299cdec8e564641a759d313
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-02-19 21:19:30 +0100

    Atualizando admin models settings e urls para o crud de receitas

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+@admin.register(Category)
+class CatergoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
