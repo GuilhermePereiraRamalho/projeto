2a8d9782e998516a900491ed51c547104d0543bd
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-02-20 21:21:53 +0100

    Ocultando receitas que não estão publicadas (filter para is_published)

--- recipes/views.py
+++ recipes/views.py
@@ -4,19 +4,19 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id+id)
+    recipe = Recipe.objects.get(id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
