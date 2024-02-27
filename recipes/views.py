1f911515dcb8e285fa3d625e8226779eef80e89f
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2024-02-27 21:29:45 +0100

    Mostrando a página de detalhes da receita

--- recipes/views.py
+++ recipes/views.py
@@ -19,7 +19,9 @@ def category(request, category_id):
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id, is_published=True)
+    recipe = Recipe.objects.filter(id=id,
+                                    is_published=True
+                                    ) .order_by('-id').first()
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
